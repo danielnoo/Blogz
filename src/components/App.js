c1472bbd58b47010192f907b4a9da16fff59f3c4
danielnoo <dannynoo@gmail.com>
2021-09-29 09:46:54 -0400

    created Nav component and placed in header, some styling, funnelled sass imports to App.scss, then to index

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
+import {useState} from 'react';
 import Publish from './Publish';
+import Nav from './Nav';
 import '../styles/App.scss';
 import firebaseApp from '../firebase'
 
 function App() {
   console.log(firebaseApp)
+  const [displayRecentPosts, setDisplayRecentPosts] = useState(false);
+  const [displayPublish, setDisplayPublish] = useState(true);
   
   return (
     <div>
       <header>
         <h1>Blogz</h1>
+        <Nav />
       </header>
       <main>
         <Publish />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,19 @@
 
 
-const Nav = () => {
+const Nav = ({loggedIn}) => {
 
+  // pass Nav info on whether or not user is logged in and conditionally render the login button as logout
 
   return(
-    //return login/out plus a big publish button
+    <nav>
+      <ul>
+        <li>{
+          loggedIn ? <button className="log-button">Login</button> : <button className="log-button">Logout</button>
+          }</li>
+        <li><button className="publish-button">Publish</button></li>
+      </ul>
+    </nav>
   )
 }
+
+export default Nav
