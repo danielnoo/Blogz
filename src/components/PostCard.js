f6d7992d13da6c5036bd3bdbcaeb507bce0b748e
danielnoo <dannynoo@gmail.com>
2021-10-01 10:26:53 -0400

    more styling on recent post cards, added fade div, added estimated time to read function

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -8,17 +8,23 @@ import {db} from '../dbFunctions';
 const PostCard = ({dbTag, title, body}) => {
 
   const handleClick = () => {
-    
+
   }
+
+  
+  const time = Math.round(body.length / 220);
+  
 
   return(
     <>
       <h3>{title}</h3>
       <p>{body}</p>
-      <button>Read more</button>
+      <div className="fade"></div>
+      <button>Read more --- {time} min. read</button>
     </>
   )
 
 };
 
 export default PostCard;
+
